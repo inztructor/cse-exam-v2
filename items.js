13792aa2a540075dfdef8f6af0ebbcfc493f8921
inztructor <rioske20p@gmail.com>
2026-09-20 12:42:51 +0800

    Update items.js

--- items.js
+++ items.js
@@ -495,8 +495,7 @@ window.onload = function() {
                 question: "If 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55, then <br>21 + 22 + 23 + 24 + 25 + 26 + 27 + 28 + 29 + 30 = ?",
                 options: ["155", "230", "255", "355"],
                 correct: 2,
-				explanation: "<b>Step-by-Step Solution</b><ol type='1'><li>Identify the shift: Each term in the new sequence is 20 more than the original term (21=1+20, 22=2+20, ..., 30=10+20).</li><li>Count the terms: There are 10 terms in total.</li><li>Calculate the additional sum: Since each of the 10 terms is increased by 20, add 10×20=200 to the original sum.</li></ol>Find the total: <b>55+200=255</b>.<br>Simple Trick!</b><br>Get the middle number from 21 to 30 which is 25.5 and multiply it by 10 (total count of numbers you are adding).<br>
-Answer is <b>255</b>."
+				explanation: "<b>Step-by-Step Solution</b><ol type='1'><li>Identify the shift: Each term in the new sequence is 20 more than the original term (21=1+20, 22=2+20, ..., 30=10+20).</li><li>Count the terms: There are 10 terms in total.</li><li>Calculate the additional sum: Since each of the 10 terms is increased by 20, add 10×20=200 to the original sum.</li></ol>Find the total: <b>55+200=255</b>.<br>Simple Trick!</b><br>Get the middle number from 21 to 30 which is 25.5 and multiply it by 10 (total count of numbers you are adding).<br>Answer is <b>255</b>."
             },
 			{
                 subject: "Mathematics",
