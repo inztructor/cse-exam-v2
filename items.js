6846439e3d5d8b3d0f71f5eb278df50bb89968a7
inztructor <rioske20p@gmail.com>
2026-09-22 15:13:30 +0800

    Update items.js

--- items.js
+++ items.js
@@ -7003,6 +7003,26 @@ window.onload = function() {
 		        options: ["Dahil", "Kong", "Kung", "Sapagkat"],
 		        correct: 2,
 		        explanation: "<b>Kung</b> ang pangatnig na ginagamit sa pagpapahayag ng kondisyon o syarat."
+		    },
+			{
+		        subject: "Filipino",
+		        subtopic: "Pagkilala sa Mali",
+		        sidebarId: "side-fil-pagk",
+		        directions: "Basahing mabuti ang bawat pangungusap. Hanapin ang salita o grupo ng mga salitang may salungguhit na di wasto ang gamit. Kung walang mali, piliin ang titik E.",
+		        question: "<u>Isinulat</u> ni Dr. Jose Rizal <u>ang nobelang</u> <u>Noli Me Tangere</u> sa <u>Fort Santiago</u>. <u>Walang mali</u>.",
+		        options: ["Isinulat", "ang nobelang", "Noli Me Tangere", "Fort Santiago", "Walang mali"],
+		        correct: 3,
+		        explanation: "Mali ang <b>Fort Santiago</b>. Isinulat at tinapos ni Dr. Jose Rizal ang nobelang Noli Me Tangere sa iba't ibang bansa sa Europa, partikular sa Madrid, Espanya at Berlin, Alemanya."
+		    },
+			{
+		        subject: "Filipino",
+		        subtopic: "Pagkilala sa Mali",
+		        sidebarId: "side-fil-pagk",
+		        directions: "Basahing mabuti ang bawat pangungusap. Hanapin ang salita o grupo ng mga salitang may salungguhit na di wasto ang gamit. Kung walang mali, piliin ang titik E.",
+		        question: "<u>Si</u> Emilio Aguinaldo <u>at</u> Manuel L. Quezon <u>ang unang<//u> dalawang pangulo <u>ng</u> Pilipinas. <u>Walang mali</u>.",
+		        options: ["Si", "at", "ang unang", "ng", "Walang mali."],
+		        correct: 0,
+		        explanation: "Mali ang <b>Si</b>, dapat itong palitan ng <b>Sina</b> dahil dalawang tao ang tinutukoy na paksa. "
 		    }
         ];
 
@@ -7015,17 +7035,7 @@ window.onload = function() {
                 question: "Look at this series: 2, 1, (1/2), (1/4), ... What number should come next?",
                 options: ["(1/3)", "(1/8)", "(1/10)", "(1/16)"],
                 correct: 1
-            },
-			{
-		        subject: "Filipino",
-		        subtopic: "Pagkilala sa Mali",
-		        sidebarId: "side-fil-pagk",
-		        directions: "Basahing mabuti ang bawat pangungusap. Hanapin ang salita o grupo ng mga salitang may salungguhit na di wasto ang gamit. Kung walang mali, piliin ang titik E.",
-		        question: "<u>Isinulat</u> ni Dr. Jose Rizal <u>ang nobelang</u> <u>Noli Me Tangere</u> sa <u>Fort Santiago</u>. <u>Walang mali</u>.",
-		        options: ["Isinulat", "ang nobelang", "Noli Me Tangere", "Fort Santiago", "Walang mali"],
-		        correct: 3,
-		        explanation: "Mali ang <b>Fort Santiago</b>. Isinulat at tinapos ni Dr. Jose Rizal ang nobelang Noli Me Tangere sa iba't ibang bansa sa Europa, partikular sa Madrid, Espanya at Berlin, Alemanya."
-		    }
+            }
         ];
 
         const generalInfoPool = [
